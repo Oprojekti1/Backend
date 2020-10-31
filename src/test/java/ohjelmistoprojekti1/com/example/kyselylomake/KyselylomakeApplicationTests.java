9cc4756ceaeb75a8fb1716250e15655b8ada1124
joona127 <joona.sorjonen@myy.haaga-helia.fi>
2020-10-31 14:56:50 +0200

    Lisäys

---
+++ src/test/java/ohjelmistoprojekti1/com/example/kyselylomake/KyselylomakeApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti1.com.example.kyselylomake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselylomakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
